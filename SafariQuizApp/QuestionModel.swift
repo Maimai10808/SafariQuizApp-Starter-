f2784d67e319f6809c3098f46b46758667b551be
Maimai10808 <331686549@qq.com>
2025-05-26 21:34:32 +0800

    1st

---
+++ SafariQuizApp/QuestionModel.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionModel.swift
+//  SafariQuizApp
+//
+//
+
+import Foundation
+import UIKit
+
+struct QuestionModel {
+    
+    let image: UIImage
+    let correctAnswer: Int
+    let answer1: String
+    let answer2: String
+    let answer3: String
+    
+}
